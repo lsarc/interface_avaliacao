6848a14dfd576ed46010d9335cac3eb8072b2bbf
Lucas S <lucas.saraceni.c@gmail.com>
2022-11-25 18:50:12 -0300

    Add project files.

---
+++ Interface/IUser.cs
@@ -0,0 +1,14 @@
+﻿using d3_avaliacao.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d3_avaliacao.Interface
+{
+    public interface IUser
+    {
+        User Read(string email, string password);
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d3_avaliacao.Model
+{
+    public class User
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string Id { get; set; } = string.Empty;
+
+    }
+}
